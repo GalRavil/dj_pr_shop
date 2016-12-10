a472d1c219ff8dfc764b365766336c28cdf7adf9
GalRavil <gal.ravil93@gmail.com>
2016-12-10 16:56:26 +0300

    + template to display the cart

--- cart/forms.py
+++ cart/forms.py
@@ -5,9 +5,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(
-        cchoice=PRODUCT_QUANTITY_CHOICES,
-        coerce=int)  # to convert the input into an integer
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
+                                      coerce=int)  # to convert the input into an integer
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
